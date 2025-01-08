fb90fcabaa4d7b5475222e681d1ad6bdafad4699
oathbreaker <azlanrasool@gmail.com>
2025-01-08 17:47:24 +0500

    Add static files for styling

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.get_months_list),
+    path("", views.get_months_list, name="index"),
     path("<int:month>", views.monthly_challenge_by_number),
     path("<str:month>", views.monthly_challenge, name="month-url")
 ]
